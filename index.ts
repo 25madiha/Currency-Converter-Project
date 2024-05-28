6b6254697cb27dd79f84684162cc9d77c4ffa622
25madiha <madiha1997@github.com>
2024-05-28 21:50:18 +0500

    push my currency converter to github

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+
+let c_value:{[key: string]: number} = {
+    PKR: 278.4,
+    UAE: 3.67,
+    USD: 1
+}
+
+let answer = await inquirer.prompt([{
+    type: 'list',
+    name: "from",
+    message: "converting from",
+    choices: ['PKR','UAE','USD']
+},
+{type: 'list',
+name: "to",
+message: "converting to",
+choices: ['PKR','UAE','USD']
+
+},
+{
+    type: "number",
+    name: "amount",
+    message: "your amount to convert"
+}
+
+])
+
+console.log(c_value[answer.to] / c_value[answer.from] * answer.amount);
